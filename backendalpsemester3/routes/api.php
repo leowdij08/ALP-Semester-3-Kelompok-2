e4e9965587d53a1be978b2507974b0f5203939bf
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-12-05 14:51:44 +0800

    minor fixes

--- backendalpsemester3/routes/api.php
+++ backendalpsemester3/routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function (Request $request) {
+    return response()->json($request);
 });
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
