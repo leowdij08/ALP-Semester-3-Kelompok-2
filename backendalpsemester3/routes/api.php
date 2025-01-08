92b033e2ab30f15dc168a0f3c9ddefda517da91f
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2025-01-08 12:34:07 +0800

    FItur search perusahaan berdasarkan namanya dan lokasinya

--- backendalpsemester3/routes/api.php
+++ backendalpsemester3/routes/api.php
@@ -24,4 +24,5 @@ Route::middleware('auth:sanctum')->group( function () {
 
 Route::controller(UserPerusahaanController::class)->prefix("perusahaan")->group(function(){
     Route::get('/{id}', "getbyID");
+    Route::get('/search/{keyword}', "search");
 });
