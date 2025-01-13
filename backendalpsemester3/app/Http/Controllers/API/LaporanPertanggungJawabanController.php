ce207fdc05a4968c08d279f3b4e5234b8e2a1079
Gibekkk <gfoucauld@student.ciputra.ac.id>
2025-01-13 18:41:00 +0800

    done

--- backendalpsemester3/app/Http/Controllers/API/LaporanPertanggungJawabanController.php
+++ backendalpsemester3/app/Http/Controllers/API/LaporanPertanggungJawabanController.php
@@ -19,8 +19,7 @@ class LaporanPertanggungjawabanController extends BaseController
     {
         try {
             if (Auth::id()) {
-                $userData = LaporanPertanggungjawaban::
-                    where('id_laporan',$id)
+                $userData = LaporanPertanggungjawaban::where('id_laporan', $id)
                     ->get()->map(
                         function ($item) {
                             return [
@@ -47,14 +46,14 @@ class LaporanPertanggungjawabanController extends BaseController
         try {
             if (Auth::id()) {
                 $dataUser = LaporanPertanggungjawaban::whereRaw("concat(id_acara, revisike) like ?", ["%$keyword%"])
-                ->get()->map(function ($item) {
-                    return [
-                        'id_acara' => $item->id_acara,
-                        'dokumentasilpj' => $item->dokumentasilpj,
-                        'diterima' => $item->diterima,
-                        'revisike' => $item->revisike,
-                    ];
-                });
+                    ->get()->map(function ($item) {
+                        return [
+                            'id_acara' => $item->id_acara,
+                            'dokumentasilpj' => $item->dokumentasilpj,
+                            'diterima' => $item->diterima,
+                            'revisike' => $item->revisike,
+                        ];
+                    });
 
                 return $this->sendResponse($dataUser, 'Report searched successfully.');
             } else {
@@ -63,73 +62,117 @@ class LaporanPertanggungjawabanController extends BaseController
         } catch (\Exception $e) {
             return $this->sendError('Server Error.', $e->getMessage(), 500);
         }
-}
+    }
 
-public function update($idAcara, Request $request): JsonResponse
-{
-    try {
-        if (Auth::id()) {
-            if (Acara::where("id_acara", $idAcara)->count() > 0) {
-                $acara = Acara::where("id_acara", $idAcara)->first();
-                if ($acara->id_organisasi == Auth::user()->id) {
-                    $validator = Validator::make($request->all(), [
-                        'dokumentasiLPJ' => 'required',
-                    ]);
+    public function update($idAcara, Request $request): JsonResponse
+    {
+        try {
+            if (Auth::id()) {
+                if (Acara::where("id_acara", $idAcara)->count() > 0) {
+                    $acara = Acara::where("id_acara", $idAcara)->first();
+                    if ($acara->id_organisasi == Auth::user()->id) {
+                        $validator = Validator::make($request->all(), [
+                            'dokumentasiLPJ' => 'required',
+                        ]);
 
-                    if ($validator->fails()) {
-                        return $this->sendError('Validation Error.', $validator->errors(), 400);
-                    }
+                        if ($validator->fails()) {
+                            return $this->sendError('Validation Error.', $validator->errors(), 400);
+                        }
 
-                    $input = $request->all();
-                    $laporan = LaporanPertanggungjawaban::where('id_acara', $idAcara);
-                    $data = [
-                        "dokumentasilpj" => $input['dokumentasiLPJ'],
-                        "revisike" => $laporan->revisike + 1
-                    ];
-                    $laporan->update($data);
-                    $dataLaporan = $laporan->get()->map(function ($item) {
-                        $userOrganisasi = $acara->organisasis;
-                        return [
-                            'id_acara' => $item->id_acara,
-                        'dokumentasilpj' => $item->dokumentasilpj,
-                        'diterima' => $item->diterima,
-                        'revisike' => $item->revisike,
+                        $input = $request->all();
+                        $laporan = LaporanPertanggungjawaban::where('id_acara', $idAcara);
+                        $data = [
+                            "dokumentasilpj" => $input['dokumentasiLPJ'],
+                            "revisike" => $laporan->revisike + 1
                         ];
-                    });
+                        $laporan->update($data);
+                        $dataLaporan = $laporan->get()->map(function ($item) {
+                            return [
+                                'id_acara' => $item->id_acara,
+                                'dokumentasilpj' => $item->dokumentasilpj,
+                                'diterima' => $item->diterima,
+                                'revisike' => $item->revisike,
+                            ];
+                        });
 
-                    return $this->sendResponse($dataLaporan, 'Report updated successfully.');
+                        return $this->sendResponse($dataLaporan, 'Report updated successfully.');
+                    } else {
+                        return $this->sendError('Forbidden.', ['error' => 'Not Your Event'], 403);
+                    }
                 } else {
-                    return $this->sendError('Forbidden.', ['error' => 'Not Your Event'], 403);
+                    return $this->sendError('Event Not Found.', ['error' => 'No Event With That ID Was Found'], 404);
                 }
             } else {
-                return $this->sendError('Event Not Found.', ['error' => 'No Event With That ID Was Found'], 404);
+                return $this->sendError('Unauthorised.', ['error' => 'Invalid Login'], 401);
             }
-        } else {
-            return $this->sendError('Unauthorised.', ['error' => 'Invalid Login'], 401);
+        } catch (\Exception $e) {
+            return $this->sendError('Server Error.', $e->getMessage(), 500);
         }
-    } catch (\Exception $e) {
-        return $this->sendError('Server Error.', $e->getMessage(), 500);
     }
-}
 
-public function delete($idAcara, Request $request): JsonResponse
-{
-    try {
-        if (Auth::id()) {
-            if (Acara::where("id_acara", $idAcara)->count() > 0) {
-                if (Acara::where("id_acara", $idAcara)->first()->id_organisasi == Auth::user()->id) {
-                    return $this->sendResponse(['isDeleted' => LaporanPertanggungjawaban::where('id_acara', $idAcara)->delete() ? true : false], 'Report deleted successfully.');
+    public function create($idAcara, Request $request): JsonResponse
+    {
+        try {
+            if (Auth::id()) {
+                if (Acara::where("id_acara", $idAcara)->count() > 0) {
+                    $acara = Acara::where("id_acara", $idAcara)->first();
+                    if ($acara->id_organisasi == Auth::user()->id) {
+                        $validator = Validator::make($request->all(), [
+                            'dokumentasiLPJ' => 'required',
+                        ]);
+
+                        if ($validator->fails()) {
+                            return $this->sendError('Validation Error.', $validator->errors(), 400);
+                        }
+
+                        $input = $request->all();
+                        $data = [
+                            "dokumentasilpj" => $input['dokumentasiLPJ'],
+                            "revisike" => 1
+                        ];
+                        $laporan = LaporanPertanggungjawaban::create($data);
+                        $dataLaporan = $laporan->get()->map(function ($item) {
+                            return [
+                                'id_acara' => $item->id_acara,
+                                'dokumentasilpj' => $item->dokumentasilpj,
+                                'diterima' => $item->diterima,
+                                'revisike' => $item->revisike,
+                            ];
+                        });
+
+                        return $this->sendResponse($dataLaporan, 'Report updated successfully.');
+                    } else {
+                        return $this->sendError('Forbidden.', ['error' => 'Not Your Event'], 403);
+                    }
                 } else {
-                    return $this->sendError('Forbidden.', ['error' => 'Not Your Event'], 403);
+                    return $this->sendError('Event Not Found.', ['error' => 'No Event With That ID Was Found'], 404);
                 }
             } else {
-                return $this->sendError('Event Not Found.', ['error' => 'No Event With That ID Was Found'], 404);
+                return $this->sendError('Unauthorised.', ['error' => 'Invalid Login'], 401);
             }
-        } else {
-            return $this->sendError('Unauthorised.', ['error' => 'Invalid Login'], 401);
+        } catch (\Exception $e) {
+            return $this->sendError('Server Error.', $e->getMessage(), 500);
         }
-    } catch (\Exception $e) {
-        return $this->sendError('Server Error.', $e->getMessage(), 500);
     }
-}
+
+    public function delete($idAcara, Request $request): JsonResponse
+    {
+        try {
+            if (Auth::id()) {
+                if (Acara::where("id_acara", $idAcara)->count() > 0) {
+                    if (Acara::where("id_acara", $idAcara)->first()->id_organisasi == Auth::user()->id) {
+                        return $this->sendResponse(['isDeleted' => LaporanPertanggungjawaban::where('id_acara', $idAcara)->delete() ? true : false], 'Report deleted successfully.');
+                    } else {
+                        return $this->sendError('Forbidden.', ['error' => 'Not Your Event'], 403);
+                    }
+                } else {
+                    return $this->sendError('Event Not Found.', ['error' => 'No Event With That ID Was Found'], 404);
+                }
+            } else {
+                return $this->sendError('Unauthorised.', ['error' => 'Invalid Login'], 401);
+            }
+        } catch (\Exception $e) {
+            return $this->sendError('Server Error.', $e->getMessage(), 500);
+        }
+    }
 }
